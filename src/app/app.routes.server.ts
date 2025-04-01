aef40b9b95e741bb952f01d9e2c809c376b2e182
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-04-01 09:51:17 +0400

    come back output mode server and add getPrerenderparams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,46 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'must-to-see/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+        { id: '6' },
+        { id: '7' },
+        { id: '8' },
+        { id: '9' },
+        { id: '10' },
+        { id: '11' },
+        { id: '12' }
+      ];
+    }
+  },
+  {
+    path: ':lang/must-to-see/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+        { id: '6' },
+        { id: '7' },
+        { id: '8' },
+        { id: '9' },
+        { id: '10' },
+        { id: '11' },
+        { id: '12' }
+      ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
