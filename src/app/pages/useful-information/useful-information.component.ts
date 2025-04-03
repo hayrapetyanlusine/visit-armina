fb8004bcde767b7d6403889c7500db90bf483204
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-04-03 15:31:50 +0400

    changes

--- src/app/pages/useful-information/useful-information.component.ts
+++ src/app/pages/useful-information/useful-information.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageHeaderComponent } from '../../ui/page-header/page-header.component';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { HtmlPipe } from '../../pipes/html.pipe';
+import { ContactModalService } from '../../services/contact-modal.service';
 
 @Component({
   selector: 'app-useful-information',
-  imports: [
-    PageHeaderComponent,
-    TranslocoPipe,
-    HtmlPipe
-  ],
+  imports: [PageHeaderComponent, TranslocoPipe, HtmlPipe],
   templateUrl: './useful-information.component.html',
   styleUrl: './useful-information.component.scss',
 })
-export default class UsefulInformationComponent {}
+export default class UsefulInformationComponent {
+  protected readonly modalService = inject(ContactModalService);
+}
