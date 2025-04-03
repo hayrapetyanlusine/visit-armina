6367ce8fb7877d22898d4839e43930f88bfee40f
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-04-03 18:16:09 +0400

    changes

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ScrollTopDirective } from '../../directives/scroll-top/scroll-top.directive';
 import { RouterLink } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { HtmlPipe } from '../../pipes/html.pipe';
 
 @Component({
   selector: 'app-footer',
-  imports: [ScrollTopDirective, RouterLink, TranslocoPipe],
+  imports: [ScrollTopDirective, RouterLink, TranslocoPipe, HtmlPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
