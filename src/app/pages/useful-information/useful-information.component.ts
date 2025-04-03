a3164053fe02f5f80a45a05bad6a3bf0c66e72fb
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2025-04-03 15:37:44 +0400

    added contact modal

--- src/app/pages/useful-information/useful-information.component.ts
+++ src/app/pages/useful-information/useful-information.component.ts
@@ -3,10 +3,11 @@ import { PageHeaderComponent } from '../../ui/page-header/page-header.component'
 import { TranslocoPipe } from '@jsverse/transloco';
 import { HtmlPipe } from '../../pipes/html.pipe';
 import { ContactModalService } from '../../services/contact-modal.service';
+import { ContactModalComponent } from '../../ui/contact-modal/contact-modal.component';
 
 @Component({
   selector: 'app-useful-information',
-  imports: [PageHeaderComponent, TranslocoPipe, HtmlPipe],
+  imports: [PageHeaderComponent, TranslocoPipe, HtmlPipe, ContactModalComponent],
   templateUrl: './useful-information.component.html',
   styleUrl: './useful-information.component.scss',
 })
